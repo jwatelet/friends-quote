1c54610e84ad45ff6292a7e2777faf26b14bc5d4
Johan Watelet <johanwatelet@gmail.com>
2022-12-17 18:07:33 +0100

    add quote creation

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,24 @@ class QuotesController < ApplicationController
   def index
     @quotes = Quote.all
   end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:text, :author)
+  end
 end
